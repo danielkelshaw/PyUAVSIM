badd1b78cda27b0bb1a50e262dbc78d7f7da376d
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-02-20 19:06:31 +0000

    Provided UAV Case Study
    Provided a file which gives example parameters for a fixed wing UAV - these can be used within the state calculations.
    
    Resolves: #2

--- uavsim/uav_dynamics.py
+++ uavsim/uav_dynamics.py
@@ -1,4 +1,31 @@
+import numpy as np
+from .parameters import uav_parameters as UAV
+
+
 class UAVDynamics:
 
-    def __init__(self):
+    def __init__(self, ts):
+
+        self.ts_sim = ts
+
+        self.state = np.array([
+            [UAV.px0],
+            [UAV.py0],
+            [UAV.pz0],
+            [UAV.u0],
+            [UAV.v0],
+            [UAV.w0],
+            [UAV.e0],
+            [UAV.e1],
+            [UAV.e2],
+            [UAV.e3],
+            [UAV.p0],
+            [UAV.q0],
+            [UAV.r0]
+        ])
+
+    def update(self, forces_moments):
+        pass
+
+    def _flight_derivatives(self, state, forces_moments):
         pass
