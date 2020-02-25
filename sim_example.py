31c4e3621432d2f34161f2d56a61406417b1eea1
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-02-25 19:36:25 +0000

    Added ability to trim aircraft
    Implemented functionality to trim aircraft based on required state.
    
    Updated the ```README.md``` with a gif to demonstrate functionality and convey the aims of this project.
    
    Resolves: #20
    Resolves: #15

--- sim_example.py
+++ sim_example.py
@@ -1,5 +1,6 @@
 import uavsim
 import numpy as np
+from uavsim.utility.trim import compute_trim
 
 
 sim_timestep = 0.01
@@ -9,22 +10,26 @@ uav = uavsim.UAVParams('params.yaml')
 uav_dynamics = uavsim.UAVDynamics(uav, sim_timestep)
 wind_sim = uavsim.WindSimulator(sim_timestep)
 
+trim_state, trim_delta = compute_trim(uav_dynamics, 25, 0)
+uav_dynamics.state = trim_state
+delta = trim_delta
+
 uav_viewer = uavsim.UAVViewer()
 
 sim_time = 0.0
-n_steps = int(15 / sim_timestep)
+n_steps = int(30 / sim_timestep)
 
 print('Starting Simulation...')
 
 n = 0
 while n < (n_steps + 1):
 
-    delta_e = -0.2
-    delta_a = 0.0
-    delta_r = 0.005
-    delta_t = 0.5
-
-    delta = np.array([[delta_e, delta_a, delta_r, delta_t]]).T
+    # delta_e = -0.2
+    # delta_a = 0.0
+    # delta_r = 0.005
+    # delta_t = 0.5
+    #
+    # delta = np.array([[delta_e, delta_a, delta_r, delta_t]]).T
 
     wind = wind_sim.update()
     uav_dynamics.update(delta, wind)
